f1934d227abeb0a6289783ea0e67601b110a2eb7
Nicolas <nicoooduran@hotmail.com>
2018-11-08 13:07:23 -0300

    Revert "Revert "Merge branch 'master' of https://github.com/NicolasDuran1/Hackaton2""
    This reverts commit 05f4f1731b39cd5b527bc84fa7bb227e74ad07c9.

--- proyectoAS04/app/Http/Controllers/DebitController.php
+++ proyectoAS04/app/Http/Controllers/DebitController.php
@@ -51,4 +51,34 @@ class DebitController extends Controller
 
         return view('debits/show', compact('debits'));
     }
+
+    public function transacciones2($id)
+    {
+        
+        $id = $id."?beginDate=01-01-2018&endDate=06-11-2018";
+        //dd($id);
+        $debits = $this->debits->all($id); 
+        //Metodo All que obtiene todos los posts. Metodo declarado en Posts.php
+
+         $ingreso= '0';
+        $egreso = '0';
+
+        foreach($debits as $d){
+            if($d->type == 'INGRESO'){
+                $ingreso = $ingreso + $d->amount;
+            }else{
+                $egreso = $egreso + $d->amount;
+            }
+        }
+
+        //Metodo All que obtiene todos los posts. Metodo declarado en Posts.php
+
+        //dd($cards);
+        //dd($id);
+
+        //dd($cards);
+
+        return view('debits/grafica', ['debits' => $debits, 'ingreso' => $ingreso, 'egreso' => $egreso]);
+
+    }
 }
